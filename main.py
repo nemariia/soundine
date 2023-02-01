e7278663f3a5e5d4096d254a07b80cadcffd1e1d
nemariia <107351924+nemariia@users.noreply.github.com>
2023-02-01 18:41:40 +0300

    remove nsound
    removed nsound because its audiostream is not thread safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
-import Nsound as ns
 
 class SoundButton():
 	pass
@@ -10,14 +9,6 @@ class MenuApp(MDApp):
 		super().__init__(**kw)
 		# loading layouts
 		self.screen = Builder.load_file("layout\\main.kv")
-		# setting an audiostreem
-		a = ns.AudioStream(44100.0)
-		# simple sine sound
-		s = ns.Sine(44100.0)
-		a << s.generate(1.0, 50.0)
-		# playback
-		ns.use("portaudio")
-		a >> ns.AudioPlayback(a.getSampleRate(), a.getNChannels(), 16)
 	def build(self):
 		self.theme_cls.primary_palette = "LightGreen"
 		return self.screen
