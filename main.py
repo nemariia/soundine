a92a10fd47bf8056a3cbb2150cde9a05ca1b5b1a
nemariia <lusuwey@gmail.com>
2023-02-02 00:57:16 +0300

    added sinewawe generator function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
+import sounddevice as sd
+import numpy as np
+import math
 
 class SoundButton():
 	pass
@@ -9,6 +12,22 @@ class MenuApp(MDApp):
 		super().__init__(**kw)
 		# loading layouts
 		self.screen = Builder.load_file("layout\\main.kv")
+		sd.default.samplerate = 44100
+		signal = self.sinewawe(150, 3)
+		sd.play(signal)
+		# get a output stream where we can play samples
+		self.stream = sd.Stream()
+
+
+	def playback(signal):
+		sd.play(signal)
+
+	def sinewawe(self, f, t, fs = 44100):
+		time = np.arange(t * fs) / fs
+		signal = np.sin(2 * np.pi * f * time)
+		return signal
+		
+
 	def build(self):
 		self.theme_cls.primary_palette = "LightGreen"
 		return self.screen
